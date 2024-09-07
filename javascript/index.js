6b1b6afc770330cdf41d5151164f704f11cd03cb
Daniel41789 <mariobros4178@gmail.com>
2024-09-06 21:48:46 -0600

    login v1.2.1

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,7 @@ document.querySelector('form').addEventListener('submit', async function (e) {
     const password = document.querySelector('#password').value;
 
     try {
-        const response = await fetch('http://127.0.0.1:3000/login', {
+        const response = await fetch('http://localhost:3000/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
